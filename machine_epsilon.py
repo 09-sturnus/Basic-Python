c7439934e23601135fccbb08e1a26fb19c1566c0
09-sturnus <ry.nagafuku@gmail.com>
2023-05-30 23:18:49 +0900

    fixed 制御構文

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,1 +1,9 @@
 # TODO
+epsilon = 1.0
+
+while True:
+    epsilon = epsilon / 2
+    if not 1 + epsilon > epsilon:
+        break
+
+print(epsilon)
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,5 +1,21 @@
-from math import sin
+from math import sin , pi
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
+
+# 積分区間
+a = 0
+b = pi / 2
+
+n = 100
+h = (b - a) / n
+
+S = 0
+
+f = sin
+for k in range(101):
+    if k != 0:
+        S += (f((k-1)*h) + f(k*h)) * h/2
+
+print(S)
