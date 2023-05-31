30631ef305ef8c5ebceaaf404184cb5c6c1602fc
09-sturnus <ry.nagafuku@gmail.com>
2023-06-01 06:03:04 +0900

    done 関数

--- euclid.py
+++ euclid.py
@@ -1,7 +1,11 @@
-a = int(input("a の値を入力: "))
-b = int(input("b の値を入力: "))
+from math import pi
+import random
+
+#a = int(input("a の値を入力: "))
+#b = int(input("b の値を入力: "))
 
 # TODO
+'''
 A = a
 B = b
 
@@ -16,4 +20,39 @@ while True:
         break
     a = b
     b = r
+'''
+
+
+def euclid(a, b):
+    if b > a:
+        a, b = b, a
     
+    while True:
+        r = a % b
+        if r == 0:
+            return b
+        a = b
+        b = r
+
+
+def is_relative(a, b):
+    if euclid(a, b) == 1:
+        return True
+    else:
+        return False
+
+
+def rand_relative():
+    rand_int = [[random.randint(1, 10000) for i in range(2)] for j in range(100000)]
+    
+    rate = 0.0
+    
+    for i in range(len(rand_int)):
+        if is_relative(rand_int[i][0], rand_int[i][1]):
+            rate += 1
+    
+    return rate / len(rand_int)
+
+
+print("Theoretical Value is %e" % (6.0 / (pi**2)))
+print("Actual Value is %e" % rand_relative())
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,9 +1,9 @@
-from math import sin , pi
+from math import sin , pi, exp
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
-
+'''
 # 積分区間
 a = 0
 b = pi / 2
@@ -19,3 +19,26 @@ for k in range(1, 1+n):
         S += (f(a + (k-1)*h) + f(a + k*h)) * h/2
 
 print(S)
+'''
+
+
+def calc_integral(f, a=0, b=1, n=100):
+    h = (b - a) / n
+    S = 0.0
+
+    for k in range(1, n+1):
+        S += (f(a + (k-1)*h) + f(a + k*h)) * h/2
+    return S
+
+
+def func1(x):
+    return 4 / (1 + x**2)
+
+
+def func2(x):
+    return (pi**0.5) * exp(-x**2)
+
+
+print(calc_integral(sin, 0, pi/2, 50))
+print(calc_integral(func1))
+print(calc_integral(func2, -100, 100, 1000))
